de1e0605fd893668bc3875d3ea093ace362d484e
Emir Mustafoski <emirmustafoski92@gmail.com>
2017-11-28 22:11:48 +0100

    Add files via upload
    Changing currency taking data from API not hard coded

--- app.js
+++ app.js
@@ -1,72 +1,46 @@
-    
 
 
-    var currencies =['USD','AUD','BGN','BRL','CAD','CHF','CNY','CZK','DKK','GBP','HKD','HRK','HUF','IDR','ILS','INR','JPY','KRW','MXN','MYR','NOK','NZD','PHP','PLN','RON','RUB','SEK','SGD','THB','TRY','EUR'];
-
-    var cur = document.getElementById('CurrencyList');
-    var fragment = document.createDocumentFragment();
-    currencies.forEach(function(currency, index){
-        var opt = document.createElement('option');
-        opt.innerHTML = currency;
-        opt.value = currency;
-        fragment.appendChild(opt);
-
-    });
-
-    cur.appendChild(fragment);
-
-    var cur2 = document.getElementById('CurrencyList2');
-    var fragment = document.createDocumentFragment();
-    currencies.forEach(function(currency, index){
-        var opt = document.createElement('option');
-        opt.innerHTML = currency;
-        opt.value = currency;
-        fragment.appendChild(opt);
-
-    });
-
-    cur2.appendChild(fragment);
-    console.log(typeof(cur));
-    console.log(fragment);
-    console.log(cur2);
-
-
-function convertCurrency(){
-    
-    var from = document.getElementById("CurrencyList").value;
-    var to = document.getElementById("CurrencyList2").value;
-    console.log(to);
-    console.log(from);
-    var xmlhttp = new XMLHttpRequest();
-    var url = 'http://api.fixer.io/latest?base=ZAR&symbols=' + from + ',' + to;
-    
-    xmlhttp.open('GET',url,true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function(){
-        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            var result = xmlhttp.responseText;
-            console.log(result);
-            var jsResult = JSON.parse(result);
-            if(jsResult.rates[to] !== NaN || undefined){
-                
-                console.log(jsResult.rates[to]);
-            }else{
-               return jsResult.rates[to] = 1;
+    function loadCurrencies(){
+        var from = document.getElementById('from');
+        var to   = document.getElementById('to');
+        var xHttp = new XMLHttpRequest();
+        xHttp.onreadystatechange=function(){
+            if(xHttp.readyState == 4 && xHttp.status==200){
+                var obj = JSON.parse(this.responseText);
+                var options = '';
+                for(key in obj.rates){
+                    options=options+ '<option>'+key+'</option>';
+                }
+                from.innerHTML = options;
+                to.innerHTML = options;
+                }
             }
-            if(jsResult.rates[from] !== NaN || undefined){
-                
-                console.log(jsResult.rates[from]);
-            }else{
-              return jsResult.rates[from] = 1;
+    xHttp.open('GET','http://api.fixer.io/latest',true);
+    xHttp.send();
+        }
+
+
+
+    function convertCurrency(){
+
+        var from = document.getElementById("from").value;
+        var to = document.getElementById("to").value;
+
+        var xmlhttp = new XMLHttpRequest();
+        var url = 'http://api.fixer.io/latest?base='+ from+ '&symbols='  + to;
+        xmlhttp.open('GET',url,true);
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function(){
+            if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+                var result = xmlhttp.responseText;
+
+
+
+                var jsResult = JSON.parse(result);
+                var amt = document.getElementById("fromAmount").value;
+                document.getElementById("toAmount").value = (jsResult.rates[to] * amt).toFixed(2);
+
+
             }
-            console.log(jsResult);
-            var oneUnit = jsResult.rates[to]/ jsResult.rates[from];
-            
-            var amt = document.getElementById("fromAmount").value;
-            document.getElementById("toAmount").value = (oneUnit * amt).toFixed(2);
-            console.log(amt);
-            console.log(oneUnit);
-            console.log(jsResult);
         }
     }
-}
